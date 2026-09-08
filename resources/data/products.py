9a364a3cff8ba7c98f00b1b0797f6af603488ed3
Jenna Kaisanlahti <jenna.kaisanlahti@iki.fi>
2026-09-08 14:26:24 +0300

    Add test data for users and products

---
+++ resources/data/products.py
@@ -0,0 +1,56 @@
+PRODUCTS = {
+    "Sauce Labs Backpack": {
+        "price": "$29.99",
+        "description": (
+            "carry.allTheThings() with the sleek, streamlined Sly Pack "
+            "that melds uncompromising style with unequaled laptop and "
+            "tablet protection."
+        ),
+        "image": "sauce-backpack-1200x1500-CjRW-Djj.jpg",
+    },
+    "Sauce Labs Bike Light": {
+        "price": "$9.99",
+        "description": (
+            "A red light isn't the desired state in testing but it sure "
+            "helps when riding your bike at night. Water-resistant with "
+            "3 lighting modes, 1 AAA battery included."
+        ),
+        "image": "bike-light-1200x1500-DxcZRFOA.jpg",
+    },
+    "Sauce Labs Bolt T-Shirt": {
+        "price": "$15.99",
+        "description": (
+            "Get your testing superhero on with the Sauce Labs bolt "
+            "T-shirt. From American Apparel, 100% ringspun combed "
+            "cotton, heather gray with red bolt."
+        ),
+        "image": "bolt-shirt-1200x1500-mR0ldpVS.jpg",
+    },
+    "Sauce Labs Fleece Jacket": {
+        "price": "$49.99",
+        "description": (
+            "It's not every day that you come across a midweight "
+            "quarter-zip fleece jacket capable of handling everything "
+            "from a relaxing day outdoors to a busy day at the office."
+        ),
+        "image": "sauce-pullover-1200x1500-BfbI-PSd.jpg",
+    },
+    "Sauce Labs Onesie": {
+        "price": "$7.99",
+        "description": (
+            "Rib snap infant onesie for the junior automation engineer "
+            "in development. Reinforced 3-snap bottom closure, "
+            "two-needle hemmed sleeved and bottom won't unravel."
+        ),
+        "image": "red-onesie-1200x1500-BrSuq0ic.jpg",
+    },
+    "Test.allTheThings() T-Shirt (Red)": {
+        "price": "$15.99",
+        "description": (
+            "This classic Sauce Labs t-shirt is perfect to wear when "
+            "cozying up to your keyboard to automate a few tests. "
+            "Super-soft and comfy ringspun combed cotton."
+        ),
+        "image": "red-tatt-1200x1500-E-qp6aYf.jpg",
+    },
+}
